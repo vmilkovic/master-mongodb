527d88461b1271d632fd0b0092e4945ff3550dba
Vedran Milković <vedran.milkovic25@gmail.com>
2021-11-20 20:14:36 +0100

    Introducing Stitch

---
+++ stitch/src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Button.css';
+
+const button = (props) => (
+  <button className='button' {...props}>
+    {props.children}
+  </button>
+);
+
+export default button;
